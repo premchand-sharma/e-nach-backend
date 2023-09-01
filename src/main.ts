86d2d0dac928ca51ddd3fb8030787e177a912ba2
vishal1145 <vishal.kumar1145@gmail.com>
2023-09-01 19:04:01 +0530

    port change

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-  console.log('Nest application is running on port 3000');
+  await app.listen(3500);
+  console.log('Nest application is running on port 3500');
 }
 bootstrap();
